e82e4330629c48d25f9b0433b8efb411eba98974
James Pier <jpier2012@gmail.com>
2024-05-12 07:55:09 -0500

    First Commit

---
+++ force-app/main/default/lwc/platformToast/platformToast.js
@@ -0,0 +1,48 @@
+import { LightningElement, track, api } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { subscribe, unsubscribe } from "lightning/empApi";
+import Id from "@salesforce/user/Id";
+
+export default class PlatformToast extends LightningElement {
+
+  @track channelName = "/event/Platform_Toast__e";
+  @api applicationName;
+  @api isUserSpecific;
+  userId = Id;
+  subscription = {};
+
+  showToast(application, title, message, variant, user) {
+    if (
+      (user == null || user === this.userId) &&
+      (application == null || application === this.applicationName)
+    ) {
+      const evt = new ShowToastEvent({
+        title: title,
+        message: message,
+        variant: variant,
+        role: 'status'
+      });
+      this.dispatchEvent(evt);
+    }
+  }
+
+  connectedCallback() {
+    const messageCallback = function(response) {
+      const message = response.data.payload;
+      this.showToast(
+        message.Application__c,
+        message.Title__c,
+        message.Message__c,
+        message.Style__c,
+        message.User__c
+      );
+    }.bind(this);
+    subscribe(this.channelName, -1, messageCallback).then(response => {
+      this.subscription = response;
+    });
+  }
+
+  disconnectedCallback() {
+    unsubscribe(this.subscription);
+  }
+}
